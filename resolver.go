d1a8f93af5948bd112a36dabc9418efcc924fa45
Heron <peng.liu@nkn.org>
2022-06-14 18:45:39 +0800

    init

---
+++ resolver.go
@@ -0,0 +1,127 @@
+package dnsresolver
+
+import (
+	"context"
+	"errors"
+	"github.com/imdario/mergo"
+	isdomain "github.com/jbenet/go-is-domain"
+	"github.com/patrickmn/go-cache"
+	"net"
+	"path"
+
+	"strings"
+	"time"
+)
+
+const (
+	PREFIX = "DNS:"
+)
+
+var (
+	// ErrInvalidDomain is returned when a string representing a domain name
+	// is not actually a valid domain.
+	ErrInvalidDomain = errors.New("not a valid domain name")
+
+	// ErrInvalidNknlink is returned when the nknlink entry in a TXT record
+	// does not follow the proper nknlink format ("nknlink=<path>")
+	ErrInvalidNknlink = errors.New("not a valid nknlink entry")
+
+	// ErrResolveFailed is returned when a resolution failed, most likely
+	// due to a network error.
+	ErrResolveFailed = errors.New("link resolution failed")
+)
+
+type Config struct {
+	Prefix       string
+	CacheTimeout time.Duration // seconds
+	DialTimeout  int           // milliseconds
+}
+
+type Resolver struct {
+	config *Config
+	cache  *cache.Cache
+}
+
+var DefaultConfig = Config{
+	Prefix:       PREFIX,
+	CacheTimeout: cache.NoExpiration,
+	DialTimeout:  5000,
+}
+
+func GetDefaultConfig() *Config {
+	return &DefaultConfig
+}
+
+func MergeConfig(config *Config) (*Config, error) {
+	merged := GetDefaultConfig()
+	if config != nil {
+		err := mergo.Merge(merged, config, mergo.WithOverride)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return merged, nil
+}
+
+func ParseTXT(txt string) (string, error) {
+	parts := strings.SplitN(txt, "=", 2)
+	if len(parts) == 2 && parts[0] == "nknlink" {
+		return path.Clean(parts[1]), nil
+	}
+
+	return "", ErrInvalidNknlink
+}
+
+func NewResolver(config *Config) (*Resolver, error) {
+	config, err := MergeConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Resolver{
+		config: config,
+		cache:  cache.New(config.CacheTimeout*time.Second, 60*time.Second),
+	}, nil
+}
+
+func (r *Resolver) Resolve(address string) (string, error) {
+	if !strings.HasPrefix(address, r.config.Prefix) {
+		return "", nil
+	}
+
+	address = address[len(r.config.Prefix):]
+	addr := address
+	addrCache, ok := r.cache.Get(address)
+	if ok {
+		addr = addrCache.(string)
+		return addr, nil
+	}
+
+	ctx := context.Background()
+	var cancel context.CancelFunc
+	if r.config.DialTimeout > 0 {
+		ctx, cancel = context.WithTimeout(ctx, time.Duration(r.config.DialTimeout)*time.Millisecond)
+		defer cancel()
+	}
+
+	if !isdomain.IsDomain(address) {
+		return "", ErrInvalidDomain
+	}
+
+	txt, err := net.LookupTXT(address)
+
+	if err != nil {
+		return "", err
+	}
+	err = ErrResolveFailed
+	for _, t := range txt {
+		p, err := ParseTXT(t)
+		if err == nil {
+			r.cache.Add(address, p, cache.DefaultExpiration)
+			return p, nil
+		}
+	}
+
+	return "", err
+}
